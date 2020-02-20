e019074d5944dbad86bfd10c14947936fe4de2e3
Darío Scattolini <dario.scattolini@gmail.com>
2020-02-20 14:06:12 +0100

    Ex2 Fase 1

--- ex2.js
+++ ex2.js
@@ -2,17 +2,25 @@ const barcelonaCenterMap = L.map('map').setView([41.387, 2.17], 16);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 20,
+    maxZoom: 19,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZHNjYXR0b2xpbmkiLCJhIjoiY2s2dW93b3JmMGIxODNnbzNkM3I0bW9ocSJ9.1qz7SprOUUh2A154p75psA'
 }).addTo(barcelonaCenterMap);
 
-const centfocsMarker = L.marker([41.387, 2.166]).addTo(barcelonaCenterMap);
+let marker;
+barcelonaCenterMap.on('click', onMapClick);
 
-centfocsMarker.bindPopup(
-    `<h3>Restaurant Centfocs</h3>
-    <p>Restaurante mediterráneo</p>
-    <p>Carrer de Balmes, 16, 08007, Barcelona</p>`
-  ).openPopup();
+function onMapClick(e) {
+    if (marker) {
+        barcelonaCenterMap.removeLayer(marker);
+    }
+    marker = L.marker(e.latlng).addTo(barcelonaCenterMap);
+    marker.bindPopup(
+        `Coordenadas:<br>
+        Lat: ${e.latlng.lat.toFixed(5)}<br>
+        Lng: ${e.latlng.lng.toFixed(5)}`
+      ).openPopup();
+    barcelonaCenterMap.flyTo(e.latlng, barcelonaCenterMap.getMaxZoom() - 1);
+}
